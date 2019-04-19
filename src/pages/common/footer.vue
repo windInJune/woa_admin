44017d08bb92d1fbed31794a8e50c8a386725364
windInJune <770371175@qq.com>
2019-04-19 15:49:18 +0800

    'S'

--- src/pages/common/footer.vue
+++ src/pages/common/footer.vue
@@ -1,15 +1,30 @@
 <template>
-  <el-footer>湘ICP备17003625号-1 湖南沃阿汇网络科技有限公司 版权所有  杭州知筑信息技术有限公司 技术支持</el-footer>
+  <el-footer>
+     <span>©2018-2019 杭州知筑信息技术有限公司 All Rights Reserved.浙ICP备140559689-1</span>
+     <img :src="imgSrc" alt="">
+  </el-footer>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      imgSrc:require('../../assets/images/bdtj.png')
+    };
+  }
+}
+</script>
 <style scoped>
-.el-footer {
+.el-footer{
   width: 100%;
-  background-color: rgb(235, 235, 235);
-  color: #999;
+  background:rgba(235,235,235,1);
+  color: #959595;
   font-size: 14px;
-  box-shadow: 0 0 8px #b4b4b4;
   line-height: 60px;
   position: absolute;
   bottom: 0;
+  z-index: 999;
+}
+.el-footer img {
+    margin-left:10px;
 }
 </style>
